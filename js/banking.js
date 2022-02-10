854757b483fe83d2cc8a4260d4adf343d87be907
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-10 21:44:31 +0600

    deposit calculation added

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,14 @@
 // Handle Deposite Button Event 
 
 document.getElementById('deposit-button').addEventListener('click', function () {
-    console.log('deposit hoise');
-})
+    // Get the ammount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmmount = depositInput.value;
+    console.log(depositAmmount);
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmmount;
+
+    // Clear the deposit input field
+    depositInput.value = '';
+});
