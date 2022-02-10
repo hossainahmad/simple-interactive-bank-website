0b984c896ed36a0379d5f2faf38ca588c02507db
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-10 21:07:21 +0600

    deposite withraw balance designed with tailwind

---
+++ js/bank.js
@@ -0,0 +1,19 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // Get users email address
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+    // Get Users password
+    const passField = document.getElementById('user-password');
+    const userPass = passField.value;
+
+    // Check users email and password
+
+    if (userEmail == 'john@demo.com' && userPass == 'secret') {
+        window.location.href = 'banking.html';
+    }
+    else {
+        alert('Enter a valid email or password');
+    }
+
+})
