5e69e62573a7bf66f813589a2b09f23d836f5cf8
hossainahmad <hossain.ahmad38@gmail.com>
2022-02-11 12:52:52 +0600

    deposit calculation and parseFloat added

--- js/banking.js
+++ js/banking.js
@@ -3,12 +3,15 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
     // Get the ammount deposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmmount = depositInput.value;
-    console.log(depositAmmount);
+    const newDepositAmmount = depositInput.value;
 
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmmount;
+    const previusDeposiAmount = depositTotal.innerText;
+    const updatedDepositTotal = parseFloat(previusDegitposiAmount) + parseFloat(newDepositAmmount);
+    console.log(updatedDepositTotal);
+    depositTotal.innerText = updatedDepositTotal;
 
     // Clear the deposit input field
     depositInput.value = '';
 });
+
